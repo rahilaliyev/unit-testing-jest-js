c0c01461a5ea358cf28b010af87df76cd9071e51
Rahil <rahil@globalsoft.az>
2023-08-30 21:31:20 +0400

    add more test for even numbers function

--- 03-even-numbers/index.js
+++ 03-even-numbers/index.js
@@ -1,3 +1,3 @@
-const evenNumbers = arr => arr.filter(num => num % 2 === 0);
+const evenNumbers = arr => arr.filter(num => typeof num === "number" && num % 2 === 0);
 
 module.exports = { evenNumbers };
--- 03-even-numbers/index.test.js
+++ 03-even-numbers/index.test.js
@@ -6,3 +6,21 @@ describe("Even numbers:", () => {
     expect(evenNumbers([7, 9, 12, 15, 18])).toEqual([12, 18]);
   });
 });
+
+describe("Even numbers:", () => {
+  test("empty array should return empty array", () => {
+    expect(evenNumbers([])).toEqual([]);
+  });
+});
+
+describe("Even numbers:", () => {
+  test("array with odd numbers should return empty array", () => {
+    expect(evenNumbers([1, 3, 5, 7, 9])).toEqual([]);
+  });
+});
+
+describe("Even numbers:", () => {
+  test("array with strings should be filtered correctly", () => {
+    expect(evenNumbers(["a", 2, "b", "c", 5, 6])).toEqual([2, 6]);
+  });
+});
