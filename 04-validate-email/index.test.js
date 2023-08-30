61a7d3764d1ea1c475be6f244f20d8c9b4912498
Rahil <rahil@globalsoft.az>
2023-08-30 21:34:22 +0400

    add more tests for validating email function

--- 04-validate-email/index.test.js
+++ 04-validate-email/index.test.js
@@ -3,6 +3,17 @@ const { validateEmail } = require("./index");
 describe("Validating email:", () => {
   test("should return true", () => {
     expect(validateEmail("example@example.com")).toBe(true);
+    expect(validateEmail("rahil.eliyev@example.com")).toBe(true);
+  });
+});
+
+describe("Validating email:", () => {
+  test("should return true", () => {
     expect(validateEmail("invalid.email")).toEqual(false);
+    expect(validateEmail(null)).toBe(false);
+    expect(validateEmail("")).toBe(false);
+    expect(validateEmail("rahil@aliyev")).toBe(false);
+    expect(validateEmail(12345)).toBe(false);
+    expect(validateEmail("!@#")).toBe(false);
   });
 });
